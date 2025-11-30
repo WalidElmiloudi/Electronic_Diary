68892ca850d69ac88560f0c4eefc820b4fee1178
WalidElmiloudi <elwalid38@gmail.com>
2025-11-30 15:38:35 +0100

    adding log out funstionality

--- index.php
+++ index.php
@@ -14,17 +14,18 @@
       <input name="password" class="w-60 h-8 border bg-gray-800 pl-2 text-xl text-green-600 border-green-600" type="password" placeholder="Password">
       <button type="submit" class="border border-green-600 text-green-600 py-1 px-2 rounded-md">LOGIN</button>
     </form>
-    <h2 class="text-xl text-green-600">or <span id="goToRegister" class="font-bold">Register</span> Now !</h2>
+    <h2 class="text-xl text-green-600">or <span id="goToRegister" class="font-bold cursor-pointer">Register</span> Now !</h2>
   </div>
   <div id="register" class="w-80 h-75 bg-gray-900 rounded-md border border-green-600 py-2 hidden flex-col justify-center items-center gap-5" aria-hidden="true">
     <h1 class="text-2xl text-green-600">REGISTER</h1>
+    
     <form class="flex flex-col justify-center items-center gap-4" action="register.php" method="post">
       <input name="username" class="w-60 h-8 border bg-gray-800 pl-2 text-xl text-green-600 border-green-600" type="text" placeholder="Username">
       <input name="email" class="w-60 h-8 border bg-gray-800 pl-2 text-xl text-green-600 border-green-600" type="email" placeholder="Email">
       <input name="password" class="w-60 h-8 border bg-gray-800 pl-2 text-xl text-green-600 border-green-600" type="password" placeholder="Password">
       <button type="submit" class="border border-green-600 text-green-600 py-1 px-2 rounded-md">REGISTER</button>
     </form>
-    <h2 class="text-xl text-green-600">or <span id="goToLogin" class="font-bold">Login</span></h2>
+    <h2 class="text-xl text-green-600">or <span id="goToLogin" class="font-bold cursor-pointer">Login</span></h2>
   </div>
   <script src="script.js"></script>
 </body>
--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (! isset($_SESSION['email'])) {
+    if (!isset($_SESSION['email'])) {
         header("Location: index.php");
         exit;
     }
@@ -14,11 +14,12 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body class="bg-black w-full h-screen">
-  <section class ="fixed bottom-0 w-full h-20 bg-gray-900 border border-green-600 flex justify-center items-center ">
+  <section class ="fixed bottom-0 w-full h-20 bg-gray-900 border border-green-600 flex justify-center items-center gap-2">
   <form class="flex gap-4" action="log.php" method = "post">
     <input type="text" name="message" class="2xl:w-200 border border-green-600 text-green-600 px-2" placeholder="message">
     <button type="submit" class="py-2 px-2 rounded-md border border-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer">Submit</button>
   </form>
+  <button class="py-2 px-2 rounded-md border border-green-600 text-green-600 hover:bg-green-600 hover:text-white cursor-pointer"><a href="logout.php">Log Out</a></button>
   </section>
   <section class =" w-full h-230 overflow-scroll [scrollbar-width:none]">
     <?php
